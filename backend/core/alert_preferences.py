de6165f050ab9ea133399edb7e6ffe8f0d4fb210
Will Allman <will.allman97@gmail.com>
2026-06-17 09:35:33 +0100

    Fix scraper dependencies and email portability

---
+++ backend/core/alert_preferences.py
@@ -0,0 +1,9 @@
+def get_alert_preferences():
+    return {
+        "email_enabled": False,
+        "min_price": None,
+        "max_price": None,
+        "min_bedrooms": None,
+        "must_have_garden": False,
+        "exclude_flats": False,
+    }
